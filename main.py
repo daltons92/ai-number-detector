6e3ad632e5f8f41492cbe7c7693b085dbe56ac0a
Dalton Shanholtz <dalton92pm@gmail.com>
2026-04-26 15:59:36 -0400

    moved model loading

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class SimpleMLP(nn.Module):
         return logits
     
 model = SimpleMLP()
+model.load_state_dict(torch.load("model.pth", weights_only=True))
 
 criterion = nn.CrossEntropyLoss()
 
@@ -89,7 +90,6 @@ torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
 
 model = SimpleMLP().to("cpu")
-model.load_state_dict(torch.load("model.pth", weights_only=True))
 
 classes = [
     "0",
